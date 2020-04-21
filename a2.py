ff527a4a9a0624b861185cfed533c80841f6114b
oddtyme <seamus@live.unc.edu>
2020-04-20 21:11:46 -0400

    Created input map

--- a2.py
+++ a2.py
@@ -14,5 +14,34 @@ input_string = f.read()
 # split lines in order to more easily handle input
 input_string_split = input_string.splitlines()
 
-for line in input_string_split
+# declare envrionment map
+env_map = {}
 
+for line in input_string_split:
+	# split line by forward slash
+	split_array = line.split('/')
+
+	# assign variables to split line
+	state = split_array[0]
+	action = split_array[1]
+	new_state = split_array[2]
+	p = split_array[3]
+
+	# check if state is in env_map
+	if state in env_map:
+		# check if action is in state
+		if action in env_map[state]:
+			# adding probability to action for state
+			evn_map[state][action][new_state] = int(p)
+		else:
+			# add action to state
+			evn_map[state][action] = {}
+			evn_map[state][action][new_state] = int(p)
+	else:
+		# add state to map
+		evn_map[state] = {}
+		evn_map[state][action] = {}
+		evn_map[state][action][new_state] = int(p)
+
+
+			
